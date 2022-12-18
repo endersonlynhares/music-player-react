30ebc519d2e6b4c6dc3b4a77a0edeed6d5a68e4f
Enderson Lynhares <enderson.linhares20@gmail.com>
2022-12-18 13:22:31 -0300

    adding a css in project

--- src/Player.js
+++ src/Player.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import useSound from "use-sound";
 import queen from "./assets/queen.mp3";
+import album from "./assets/album_queen.jpg";
 import { AiFillPlayCircle, AiFillPauseCircle } from "react-icons/ai";
 import { BiSkipNext, BiSkipPrevious } from "react-icons/bi";
 import { IconContext } from "react-icons";
@@ -8,6 +9,16 @@ import { IconContext } from "react-icons";
 const Player = () => {
   const [isPlaying, setIsPlaying] = useState(false);
   const [play, { pause, duration, sound }] = useSound(queen);
+  const [time, setTime] = useState({
+    min: "",
+    sec: "",
+  });
+  const [currTime, setCurrTime] = useState({
+    sec: "",
+    min: "",
+  });
+  const [seconds, setSeconds] = useState();
+
   const playingButton = () => {
     if (isPlaying) {
       pause();
@@ -18,39 +29,89 @@ const Player = () => {
     }
   };
 
+  useEffect(() => {
+    const sec = duration / 1000;
+    const min = Math.floor(sec / 60);
+    const secRemain = Math.floor(sec % 60);
+    setTime({
+      min: min,
+      sec: secRemain,
+    });
+  });
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (sound) {
+        setSeconds(sound.seek([])); // setting the seconds state with the current state
+        const min = Math.floor(sound.seek([]) / 60);
+        const sec = Math.floor(sound.seek([]) % 60);
+        setCurrTime({
+          min,
+          sec,
+        });
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [sound]);
+
   return (
     <div className="component">
       <h2>Playing Now</h2>
-      <img src="" alt="" />
-      <div>
+      <img src={album} width="300" alt="" />
+      <div className="field__information">
         <h3 className="title">Love of my Life</h3>
         <p className="subTitle">Queen</p>
       </div>
-      <div className="">
-        <button className="playButton">
-          <IconContext.Provider value={{ size: "3em", color: "#27ae60" }}>
+      <div className="field__option">
+        <button className="play__button">
+          <IconContext.Provider value={{ size: "3em", color: "#ececec" }}>
             <BiSkipPrevious />
           </IconContext.Provider>
         </button>
         {!isPlaying && (
-          <button className="playButton" onClick={playingButton}>
-            <IconContext.Provider value={{ size: "3em", color: "#27ae60" }}>
+          <button
+            className="play__button button_function"
+            onClick={playingButton}
+          >
+            <IconContext.Provider value={{ size: "3em", color: "#fff" }}>
               <AiFillPlayCircle />
             </IconContext.Provider>
           </button>
         )}
         {isPlaying && (
-          <button className="playButton" onClick={playingButton}>
-            <IconContext.Provider value={{ size: "3em", color: "#27ae60" }}>
+          <button
+            className="play__button button_function"
+            onClick={playingButton}
+          >
+            <IconContext.Provider value={{ size: "3em", color: "#fff" }}>
               <AiFillPauseCircle />
             </IconContext.Provider>
           </button>
         )}
-        <button className="playButton">
-          <IconContext.Provider value={{ size: "3em", color: "#27ae60" }}>
+        <button className="play__button">
+          <IconContext.Provider value={{ size: "3em", color: "#ececec" }}>
             <BiSkipNext />
           </IconContext.Provider>
         </button>
+
+        <div>
+          <span className="time">
+            {currTime.min}:{currTime.sec}
+          </span>
+          <input
+            type="range"
+            min="0"
+            max={duration / 1000}
+            default="0"
+            value={seconds}
+            className="timeline"
+            onChange={(e) => {
+              sound.seek([e.target.value]);
+            }}
+          />
+          <span className="time">
+            {time.min}:{time.sec}
+          </span>
+        </div>
       </div>
     </div>
   );
